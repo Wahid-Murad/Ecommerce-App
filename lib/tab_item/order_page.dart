369f66a6e5ee3c43b06e6f9a48a0ffd03492bf61
Wahid-Murad <70386555+Wahid-Murad@users.noreply.github.com>
2022-11-14 21:17:40 +0600

    Update order_page.dart

--- lib/tab_item/order_page.dart
+++ lib/tab_item/order_page.dart
@@ -13,7 +13,33 @@ class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: orderList.length,
+          itemBuilder: (context,index){
+            return Card(
+              color: Colors.white70,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                 Text("Name: ${orderList[index].user!.name}",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w400,color: Colors.black87),),
+                 Text("id: ${orderList[index].id}",), 
+                 Text("price: ${orderList[index].price}",), 
+                 Text("quantity: ${orderList[index].quantity}",), 
+                 Text("discount: ${orderList[index].discount}",), 
+                 Text("vat: ${orderList[index].vat}",),  
+                 Text("Order ststus: ${orderList[index].orderStatus!.orderStatusCategory!.name}"),
+                 Text("date and time: ${orderList[index].orderDateAndTime}",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400,color: Colors.black54),),
+                 SizedBox(
+                  height: 5,
+                 ),
+                ],
+              ),
+            );
+          }
+          ),
+      ),
     );
   }
 }
